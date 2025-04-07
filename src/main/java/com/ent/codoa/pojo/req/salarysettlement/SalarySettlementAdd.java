b59d210e5be109cc5393494603c79fd514971b7e
churchill <asdf@@123>
2025-04-07 18:51:58 +0800

    修改bug

--- src/main/java/com/ent/codoa/pojo/req/salarysettlement/SalarySettlementAdd.java
+++ src/main/java/com/ent/codoa/pojo/req/salarysettlement/SalarySettlementAdd.java
@@ -20,22 +20,22 @@ public class SalarySettlementAdd implements Serializable {
     @ApiModelProperty(value = "员工账号",required = true)
     private String account;
 
-    @ApiModelProperty("基本工资")
-    private BigDecimal baseSalary;
+    @ApiModelProperty(value = "基本工资")
+    private BigDecimal baseSalary = BigDecimal.ZERO;;
 
     @NotNull(message = "工作日加班费比例不能为空")
     @ApiModelProperty(value = "工作日加班费比例  例如:1就是没有 1.1就是1.1倍的意思, 2就是两倍的意思",required = true)
-    private BigDecimal weekdayOvertimePayRate;
+    private BigDecimal weekdayOvertimePayRate = BigDecimal.ZERO;;
 
     @ApiModelProperty("平日加班费")
-    private BigDecimal weekdayOvertimeAmount;
+    private BigDecimal weekdayOvertimeAmount = BigDecimal.ZERO;;
 
     @NotNull(message = "周末加班费用比例不能为空")
     @ApiModelProperty(value = "周末加班费比例 例如:1就是没有 1.1就是1.1倍的意思, 2就是两倍的意思",required = true)
-    private BigDecimal weekendOvertimePayRate;
+    private BigDecimal weekendOvertimePayRate = BigDecimal.ZERO;
 
     @ApiModelProperty("周末加班费")
-    private BigDecimal weekendOvertimeAmount;
+    private BigDecimal weekendOvertimeAmount = BigDecimal.ZERO;;
 
     @NotNull(message = "应该出勤天数不能为空")
     @ApiModelProperty(value = "应该出勤天数",required = true)
@@ -52,7 +52,7 @@ public class SalarySettlementAdd implements Serializable {
     private BigDecimal lateHours = BigDecimal.ZERO;
 
     @ApiModelProperty("早退时间总计 单位小时")
-    private BigDecimal leaveEarly;
+    private BigDecimal leaveEarly = BigDecimal.ZERO;;
 
     @ApiModelProperty("平日加班时间 单位小时")
     private BigDecimal weekdaysOvertime = BigDecimal.ZERO;
@@ -69,20 +69,20 @@ public class SalarySettlementAdd implements Serializable {
     @ApiModelProperty("绩效评分")
     private String performanceScore;
 
-    @ApiModelProperty("所得税交税比例")
-    private Integer payTaxesRate;
+    @ApiModelProperty(value = "所得税交税比例",required = true)
+    private Integer payTaxesRate = 0;
 
-    @ApiModelProperty("所得税交税金额")
-    private BigDecimal payTaxesAmount;
+    @ApiModelProperty(value = "所得税交税金额",required = true)
+    private BigDecimal payTaxesAmount = BigDecimal.ZERO;;
 
     @ApiModelProperty("住民税比例")
-    private Integer residentTaxRate;
+    private Integer residentTaxRate = 0;
 
     @ApiModelProperty("住民税金额")
-    private BigDecimal residentTaxAmount;
+    private BigDecimal residentTaxAmount = BigDecimal.ZERO;;
 
     @ApiModelProperty("估算工资")
-    private BigDecimal estimateSalary;
+    private BigDecimal estimateSalary = BigDecimal.ZERO;;
 
 
 
